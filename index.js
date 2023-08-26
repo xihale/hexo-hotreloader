2aff5fa562bd46955b943ce5b1beca193e4e74c9
xihale <xihale.top@qq.com>
2023-08-26 15:43:31 +0800

    fix: connection close event

--- index.js
+++ index.js
@@ -9,24 +9,31 @@ const wss = new WebSocket.Server({ port: 3000 });
 
 console.log("hexo-hotreloader listening on ws://localhost:3000");
 
-var ws_clients={};
+var ws_clients={}, cnt={};
 
 wss.addListener("connection", ws => {
   ws.addEventListener("message", msg => {
     console.log(msg.data+" connected");
     if(!ws_clients.hasOwnProperty(msg.data))
-      ws_clients[msg.data]=[];
+      ws_clients[msg.data]=[], cnt[msg.data]=0;
     ws_clients[msg.data].push(ws);
+    ++cnt[msg.data];
   })
 });
 
 hexo.extend.filter.register(
   "after_post_render",
   data=>{
-    // 发送信息给客户端 
     if(ws_clients.hasOwnProperty(data?.title)){
-      ws_clients[data?.title].forEach(ws=>ws.send(data.content));
-    }else data.content+="<script>"+frontend+"</script>";
+      ws_clients[data?.title].forEach(ws=>{
+        try{
+          ws.send(data.content)
+        }catch(_){ // connection closed 
+          --cnt[data?.title];
+        }
+      });
+    }
+    if(cnt[data?.title]===undefined || cnt[data?.title]===0) data.content+="<script>"+frontend+"</script>";
     return data;
   },
   30
